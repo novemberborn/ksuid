2bc2952f407eb16f4b12606bfa2a8947c09b6360
Olivier Refalo <orefalo@yahoo.com>
2017-06-26 14:39:31 -0400

    adjust typing based on recommendations

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 /// <reference types="node" />
 export declare class KSUID {
-    constructor(buffer: any);
+    constructor(buffer: Buffer);
     readonly date: Date;
     readonly timestamp: number;
     readonly payload: Buffer;
     readonly string: string;
-    compare(other: KSUID): any;
+    compare(other: KSUID): number;
     equals(other: KSUID): boolean;
     toString(): string;
     static random(): Promise<KSUID>;
     static randomSync(): KSUID;
-    static fromParts(timeInMs: any, payload: any): KSUID;
+    static fromParts(timeInMs: number, payload: Buffer): KSUID;
     static isValid(buffer: Buffer): boolean;
     static parse(str: string): KSUID;
 }
